69bedf9a27fcd61267230bafbfaa68eba3b8c14c
Dewi Safira Ramadhani <safira.ramadhani@zettacamp.pro>
2022-10-11 15:42:09 +0700

    update

--- bookStock.js
+++ bookStock.js
@@ -9,7 +9,8 @@ const tax = 10/100;
 function purchasing(name, price, stock, purchased, disc, tax){
     if(stock!=0){
         console.log("The book is available\n");
-            for(i=0; i<=stock; i--){
+        for(i=0; i<=purchased; i++){
+            console.log("----------- Purchasing #" + (i+1) + "-----------\n");
             console.log("Book : " + name);
             console.log("Price : " + price);
             console.log("Discount : " + price*disc + " (" + disc*100 + "%)");
@@ -26,9 +27,11 @@ function purchasing(name, price, stock, purchased, disc, tax){
 
             console.log("Purchased :" + purchased);
             if(stock!=0){
-                console.log("\nThe book still can be purchased");
+                console.log("\nThe book still can be purchased\n");
+                console.log("----------------------------------\n");
             }else{
-                console.log("The book is out of stock");
+                console.log("----------------------------------\n");
+                console.log("\nThe book is out of stock");
                 break;
             }
         }
