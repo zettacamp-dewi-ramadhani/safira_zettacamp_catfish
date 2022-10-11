5ad578306bb2a9cd327de64fcea1e7df9a2637ac
Dewi Safira Ramadhani <safira.ramadhani@zettacamp.pro>
2022-10-11 14:13:13 +0700

    first commit

---
+++ bookStock.js
@@ -0,0 +1,40 @@
+var bookName = "Book A";
+let bookPrice = 100000;
+const bookStock = 10;
+const bookPurchased = 5;
+
+const disc = 15/100;
+const tax = 10/100;
+
+function purchasing(name, price, stock, purchased, disc, tax){
+    if(stock!=0){
+        console.log("The book is available\n");
+            for(i=0; i<=stock; i--){
+            console.log("Book : " + name);
+            console.log("Price : " + price);
+            console.log("Discount : " + price*disc + " (" + disc*100 + "%)");
+            pad = price*(1-disc);
+            console.log("Price after discount : Rp" + pad);
+
+            console.log("Tax : " + pad*tax + " (" + tax*100 + "%)");
+            pat = pad+(pad*tax);
+            console.log("Total : Rp" + pat);
+
+            stock=stock-1;
+            console.log("Stock :" + stock);
+            purchased=purchased+1;
+
+            console.log("Purchased :" + purchased);
+            if(stock!=0){
+                console.log("\nThe book still can be purchased");
+            }else{
+                console.log("The book is out of stock");
+                break;
+            }
+        }
+    }else{
+        console.log("The book is out of stock");
+    }
+}
+
+purchasing(bookName, bookPrice, bookStock, bookPurchased, disc, tax);
