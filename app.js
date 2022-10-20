84c1a5db692ed222ede903ca986dd34b31047e24
Dewi Safira Ramadhani <safira.ramadhani@zettacamp.pro>
2022-10-20 13:58:46 +0700

    add json package and update the task

--- app.js
+++ app.js
@@ -1,3 +1,61 @@
+const express = require ('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const tokenSecret = 'secret';
+const multer = require('multer');
+const forms = multer();
+
+app.use(bodyParser.json());
+app.use(forms.array());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/',(req, res)=>{
+    const uname = req.body.username;
+    console.log(uname);
+    const pass = req.body.password;
+    console.log(pass);
+
+    const token = jwt.sign({
+        username : uname, password: pass},
+        tokenSecret,{expiresIn: '1h'});
+    res.json(token);
+});
+
+const authJwt = (req, res, next)=>{
+    const auth = req.headers.authorization;
+    const access = auth.split(' ')[1];
+    if(access == null){
+        return res.sendStatus(401);
+    }else{
+        jwt.verify(access, tokenSecret, (err, user)=>{
+            if(err){
+                return res.sendStatus(403);
+            }else{
+                return next();
+            }
+        })
+    }
+}
+
+app.get('/artist', authJwt, (req,res)=>{
+    let result = artist("Blackpink")
+    // console.log(result);
+    res.send(result);
+});
+
+app.get('/genre', authJwt, (req,res)=>{
+    res.send(genreGroup);
+});
+
+app.get('/playlist', authJwt, async (req,res)=>{
+    let result = await playlist();
+    // console.log(result);
+    res.send(result);
+})
+
+app.listen(3000);
+
 let songList = [
     // 1
     {
@@ -7,22 +65,23 @@ let songList = [
         "genre" : "Pop",
         "duration" : "3.50"
     },
+    //2
     {
         "titile" : "Overpass Graffiti",
         "artist" : "Ed Sheeran",
         "album" : "Equal",
         "genre" : "Pop",
-        "duration" : "3.56"
+        "duration" : "4.56"
     },
-    // 2
+    // 3
     {
         "titile" : "Lovesick Girls",
         "artist" : "Blackpink",
         "album" : "The Album",
         "genre" : "K-pop",
-        "duration" : "4.12"
+        "duration" : "4.17"
     },
-    // 3
+    // 4
     {
         "titile" : "Grand Escape",
         "artist" : "RADWIMPS, Toko Miura",
@@ -30,31 +89,31 @@ let songList = [
         "genre" : "J-Pop",
         "duration" : "5.39"
     },
-    // 4
+    // 5
     {
         "titile" : "Clarity",
         "artist" : "Zedd, Foxes",
         "album" : "Clarity",
         "genre" : "Dance",
-        "duration" : "4.31"
+        "duration" : "4.36"
     },
-    // 5
+    // 6
     {
         "titile" : "Firework",
         "artist" : "Katy Perry",
         "album" : "Teenage Dream",
         "genre" : "Pop",
-        "duration" : "3.47" 
+        "duration" : "5.47" 
     },
-    // 6
+    // 7
     {
         "titile" : "I Can't Stop Me",
         "artist" : "TWICE",
         "album" : "Eyes Wide Open",
         "genre" : "K-pop",
-        "duration" : "30.25"
+        "duration" : "4.25"
     },
-    // 7
+    // 8
     {
         "titile" : "Pretender",
         "artist" : "Hige DANdism",
@@ -62,45 +121,47 @@ let songList = [
         "genre" : "J-Pop",
         "duration" : "5.26"
     },
-    // 8
+    // 9
     {
         "titile" : "Heroes (we could be)",
         "artist" : "Alesso, Tove Lo",
         "album" : "Forever",
         "genre" : "Dance",
-        "duration" : "30.30",
+        "duration" : "5.35",
     },
-    // 9
+    // 10
     {
         "titile" : "Unconditionally",
         "artist" : "Katy Perry",
         "album" : "PRISM",
         "genre" : "Pop",
-        "duration" : "3.48" 
+        "duration" : "4.48" 
     },
-    // 10
+    // 11
     {
         "titile" : "Ready For Love",
         "artist" : "Blackpink",
         "album" : "Born Pink",
         "genre" : "K-pop",
-        "duration" : "3.04"
+        "duration" : "10.00"
     },
+    // 12
     {
         "titile" : "More & More",
         "artist" : "TWICE",
         "album" : "More & More",
         "genre" : "K-pop",
-        "duration" : "3.19"
+        "duration" : "5.19"
     },
+    //13
     {
         "titile" : "XOXO",
         "artist" : "Jeon Somi",
         "album" : "XOXO",
         "genre" : "K-pop",
-        "duration" : "3.27"
+        "duration" : "4.27"
     },
-    // 11
+    // 14
     {
         "titile" : "Celebration",
         "artist" : "RADWIMPS, Toko Miura",
@@ -108,21 +169,26 @@ let songList = [
         "genre" : "J-Pop",
         "duration" : "4.34"
     },
-    // 12
+    // 15
     {
-        "titile" : "In the Name of Love",
-        "artist" : "Martin Garrix, Bebe Rexha",
-        "album" : "In the Name of Love",
-        "genre" : "Dance",
-        "duration" : "3.15",
+        "titile" : "Coin",
+        "artist" : "IU",
+        "album" : "Lilac",
+        "genre" : "K-pop",
+        "duration" : "5.18",
     }
 ];
 
 // group based artist
 function artist(artist){
+    let obj = {};
     const artistGroup = songList.filter(song => song.artist == artist);
-    console.log("---------------Song List of " + artist + "---------------");
-    console.log(artistGroup);
+    const map = new Map;
+    map.set(artist, artistGroup);
+    map.forEach((v,k)=>{
+        obj[k] = v;
+    });
+    return obj;
 }
 
 // group based genre
@@ -133,15 +199,32 @@ const genreGroup = songList.reduce((group, song) => {
     return group;
 }, {});
 
+let randomArray = (x)=>{
+    let arr = [];
+    while(x.length !== 0){
+        let randomIndex = Math.floor(Math.random()*x.length);
+        arr.push(x[randomIndex]);
+        x.splice(randomIndex, 1);
+    }
+    x = arr;
+    return x;
+}
+
 // playlist less than 1 hour
-function playlist(){
+async function playlist(){
     let songIndex = 0;
     let temp=0;
+    let set = new Set();
+    let map = new Map();
+    let obj = {};
 
-    for (let i = 0; i < songList.length; i++) {
-        time = songList[i].duration.split(".");
+    let song = await randomArray(songList);
+    // return song[0];
+
+    for (let i = 0; i < song.length; i++) {
+        time = song[i].duration.split(".");
         let menit = parseInt(time[0])*60;        
-        let detik = parseInt(time[1])*1;
+        let detik = parseInt(time[1]);
         temp = temp + (menit+detik); 
         if(temp < 3600){
             songIndex++;
@@ -149,12 +232,18 @@ function playlist(){
     }
 
     for (let i = 0; i < songIndex; i++) {
-       console.log(songList[i]);        
+        // return song;
+        set.add(song[i]);            
     }
-}
+    // return set;
+    
+    let [...data] = set;
+    for(i=0; i<data.length; i++){
+        map.set(i+1, data[i]);
+    }
 
-artist("Blackpink");
-console.log("\n---------------Song List Based of Genre---------------");
-console.log(genreGroup);
-console.log("\n---------------Playlist Less than 1 hour---------------");
-playlist()
+    map.forEach((v,k)=>{
+        obj[k] = v;
+    });
+    return obj;
+}
