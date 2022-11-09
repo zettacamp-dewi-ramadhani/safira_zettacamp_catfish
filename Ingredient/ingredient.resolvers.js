0b03640b132fe918d3edcec3dafc1834b2d296aa
Dewi Safira Ramadhani <safira.ramadhani@zettacamp.pro>
2022-11-09 13:15:00 +0700

    add insert data

--- Ingredient/ingredient.resolvers.js
+++ Ingredient/ingredient.resolvers.js
@@ -1,10 +1,69 @@
 const Ingredient = require('./ingredient.model');
 
-const getAllIngredients = async(parent, {filter})=>{
+const insertIngredient = async(parent, {input})=>{
+    if(!input){
+        console.log("Nothing to insert")
+    }else{
+        const {name, stock} = input;
+        let data = new Ingredient({
+            name : name,
+            stock : stock
+        });
+        await data.save();
+        return data;
+    }
+}
 
+const getAllIngredients = async(parent, {filter})=>{
+    const {name, stock, paging} = filter;
+    if(!name && !stock){
+        let result = await Ingredient.aggregate([{
+            $skip : paging.page * paging.limit
+        },{
+            $limit : paging.limit
+        }]);
+        return result;
+    }else if(name && !stock){
+        let result = await Ingredient.aggregate([{
+            $match : {
+                name : name
+            }
+        },{
+            $skip : paging.page * paging.limit
+        },{
+            $limit : paging.limit
+        }])
+        return result;
+    }else if(!name && stock>0){
+        let result = await Ingredient.aggregate([{
+            $match : {
+                stock  : {
+                    $gte : stock
+                }
+            }
+        },{
+            $skip : paging.page * paging.limit
+        },{
+            $limit : paging.limit
+        }]);
+        return result;
+    }else if(name && stock>0){
+        let result = await Ingredient.aggregate([{
+            $match : {
+                name : name,
+                stock  : {
+                    $gte : stock
+                }
+            }
+        },{
+            $skip : paging.page * paging.limit
+        },{
+            $limit : paging.limit
+        }]);
+        return result;
+    }
 }
 const getOneIngredient = ()=>{}
-const insertIngredient = ()=>{}
 const updateIngredient = ()=>{}
 const deleteIngredient = ()=>{}
 
