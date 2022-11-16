a887056a48ffd50bcb4a618786e72d8f38f70aca
zettacamp-Muhammad-Rizqi <muhammad.rizqi@zettacamp.pro>
2022-11-16 11:47:50 +0700

    Done make it work the database

--- Controller/database.js
+++ Controller/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect('mongodb://localhost:27017/zettacamp_batch3', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connect('mongodb://127.0.0.1:27017/zettacamp_batch3', { useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>console.log("Connected"))
 .catch((err)=>console.log(err));
 
