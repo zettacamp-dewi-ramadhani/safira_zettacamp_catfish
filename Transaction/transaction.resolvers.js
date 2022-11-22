7d2773916e8a4d31265350918bebce32abe4d66a
zettacamp_dewi_ramadhani <safira.ramadhani@zettacamp.pro>
2022-11-22 10:56:04 +0700

    add count data

--- Transaction/transaction.resolvers.js
+++ Transaction/transaction.resolvers.js
@@ -1,374 +1,423 @@
-const Transaction = require('./transaction.model');
-const Ingredient = require('../Ingredient/ingredient.model');
-const Recipe = require('../Recipe/recipe.model');
-const moment = require('moment');
-const mongoose = require('mongoose');
+const Transaction = require("./transaction.model");
+const Ingredient = require("../Ingredient/ingredient.model");
+const Recipe = require("../Recipe/recipe.model");
+const moment = require("moment");
+const mongoose = require("mongoose");
 
-moment.locale('id-ID');
+moment.locale("id-ID");
 
-const reduceIngredientStock  = async (menu)=>{
-    for(const recipe of menu){
-        recipeData = await Recipe.findOne({
-            _id : recipe.recipe_id
-        });
-        for(const ingredient of recipeData.ingredients){
-            const amountData = ingredient.stock_used*recipe.amount;
-            ingredientData = await Ingredient.updateOne({
-                _id : ingredient.ingredient_id,
-                stock : {
-                    $gte : 0
-                }
-            },{
-                $inc : {
-                    stock : - amountData
-                }
-            });
-        }
-    }
-    return true;
-}
-
-const validateStockIngredient = async(menu)=>{
-    let recipeData = [];
-    let ingredientData = [];
-    let available = []
-    for(const recipe of menu){
-        recipeData = await Recipe.findOne({
-            _id : recipe.recipe_id
-        });
-        for(const ingredient of recipeData.ingredients){
-            ingredientData = await Ingredient.findOne({
-                _id : ingredient.ingredient_id,
-                stock : {
-                    $gte : 0
-                }
-            });
-            if(ingredientData.stock >= (ingredient.stock_used*recipe.amount)){
-                available.push(true)
-            }else{
-                available.push(false)
-            }
-        }
-    }
-    const temp = available.includes(false);
-    if(temp === false){
-        await reduceIngredientStock(menu);
-        return true;
-        
-    }else{
-        return false;
-    }
-}
-
-const getUserLoader = async(parent, args, ctx)=>{
-    if(parent.user_id){
-        const result = await ctx.dataUserLoader.load(parent.user_id);
-        return result;
-    }
-}
-
-const getMenuLoader = async(parent, args, ctx)=>{
-    if(parent.recipe_id){
-        const result = await ctx.dataRecipeLoader.load(parent.recipe_id);
-        return result;
-    }
-}
-
-const getTotalPrice = async(menu)=>{
-    let total = [];
-    let totalPrice = []
-    for(recipe of menu){
-        recipeData = await Recipe.findOne({
-            _id : recipe.recipe_id
-        });
-        total.push(recipe.amount * recipeData.price);
-        totalPrice = total.reduce((a,b)=>a+b);
-    }
-    return totalPrice;
-}
-
-const updateMenu = async(id, menu)=>{
-    let totalPrice = await getTotalPrice(menu);
-    const addMenu = await Transaction.findByIdAndUpdate({
-        _id : id
-    },{
-        $push : {
-            menu : menu
-        }
-    },{
-        new : true
+const reduceIngredientStock = async menu => {
+  for (const recipe of menu) {
+    recipeData = await Recipe.findOne({
+      _id: recipe.recipe_id
     });
-    if(addMenu){
-        const updateTotal = await Transaction.findByIdAndUpdate({
-            _id : id
-        },{
-            $inc : {
-                total : totalPrice
-            }
-        },{
-            new : true
-        });
-        return updateTotal
-    }
-}
-
-const createTransaction = async(id, menu)=>{
-    let totalPrice = await getTotalPrice(menu);
-    if(id && menu){
-        let data = new Transaction({
-            user_id : id,
-            menu : menu,
-            total : totalPrice,
-            order_status : 'pending',
-            status : 'active'
-        });
-        await data.save();
-        return data;
-    }else{
-        throw new Error('Cant create new transaction');
-    }
-}
-
-const addCart = async(parent, {input}, ctx)=>{
-    const userId = ctx.user[0]._id;
-    const data = await Transaction.findOne({
-        user_id : mongoose.Types.ObjectId(userId),
-        order_status : 'pending',
-        status : 'active'
-    });
-    if(!input){
-        throw new Error('No data to input');
-    }else{
-        const {menu} = input
-        if(data == null){
-            const create = await createTransaction(userId, menu);
-            return create
-        }else{
-            const update = await updateMenu(data._id, menu);
-            return update
+    for (const ingredient of recipeData.ingredients) {
+      const amountData = ingredient.stock_used * recipe.amount;
+      ingredientData = await Ingredient.updateOne(
+        {
+          _id: ingredient.ingredient_id,
+          stock: {
+            $gte: 0
+          }
+        },
+        {
+          $inc: {
+            stock: -amountData
+          }
         }
+      );
     }
-}
-
-const deleteMenu = async(parent, {input}, ctx)=>{
-    if(!input){
-        throw new Error('No menu to dalete');
-    }else{
-        const userId = ctx.user[0]._id;
-        const data = await Transaction.findOne({
-            user_id : mongoose.Types.ObjectId(userId),
-            order_status : 'pending',
-            status : 'active'
-        });
-        if(data){
-            const {id} = input
-            const deleteMenu = await Transaction.findByIdAndUpdate({
-                _id : data._id
-            },{
-                $pull : {
-                    menu : {
-                        _id : mongoose.Types.ObjectId(id)
-                    }
-                }
-            },{
-                new : true
-            });
-            if(deleteMenu){
-                let totalPrice = await getTotalPrice(deleteMenu.menu);
-                const updateTotal = await Transaction.findByIdAndUpdate({
-                    _id : data._id
-                },{
-                    $set : {
-                        total : totalPrice
-                    }
-                },{
-                    new : true
-                });
-                return updateTotal
-            }
-        }
-    }
-}
-
-const updateOrderStatus = async(parent, args, ctx)=>{
-    const userId = ctx.user[0]._id;
-    const data = await Transaction.findOne({
-        user_id : mongoose.Types.ObjectId(userId),
-        order_status : 'pending',
-        status : 'active'
-    });
-
-    if(data != null){
-        const validate = await validateStockIngredient(data.menu);
-        if(validate === true){
-            const result = await Transaction.findByIdAndUpdate({
-                _id : data._id
-            },{
-                $set : {
-                    order_status : 'success'
-                }
-            },{
-                new : true
-            });
-            return result;
-        }else{
-            const result = await Transaction.findByIdAndUpdate({
-                _id : data._id
-            },{
-                $set : {
-                    order_status : 'failed'
-                }
-            },{
-                new : true
-            });
-            return result;
-        }
-    }else{
-        throw new Error('Cant update order status')
-    }
-}
-
-const getAllTransactions = async(parent, {filter, pagination,order_status}, ctx)=>{
-    let aggregateQuery = [];
-    let matchQuerry = {
-        $and : [{
-            status : 'active'
-        }],
-    }
-    
-    if(filter){
-        if(filter.user_lname || filter.recipe_name){
-            if(filter.user_lname){
-                const search = new RegExp(filter.user_lname, 'i');
-                aggregateQuery.push({
-                    $lookup : {
-                        from : 'users',
-                        localField : 'user_id',
-                        foreignField : '_id',
-                        as : 'user_detail'
-                    }
-                },{
-                    $match :{
-                        'user_detail.last_name' : search,
-                        }
-                })
-            }
-
-            if(filter.recipe_name){
-                const search = new RegExp(filter.recipe_name, 'i');
-                aggregateQuery.push({
-                    $lookup : {
-                        from : 'recipes',
-                        localField : 'menu.recipe_id',
-                        foreignField : '_id',
-                        as : 'recipe_detail'
-                    }
-                },{
-                    $match : {
-                        "recipe_detail.recipe_name" : search,
-                        }
-                }) 
-            }
-        }
-    
-        if(filter.order_status){
-            const search = new RegExp(filter.order_status, 'i');
-            matchQuerry.$and.push({
-                order_status : search
-            })
-        }
-    
-        if(filter.order_date){
-            const search = new RegExp(filter.order_date, 'i');
-            matchQuerry.$and.push({
-                order_date : search
-            });
-        }
-    }
-    
-    if(matchQuerry.$and.length){
-        aggregateQuery.push({
-          $match: matchQuerry
-        })
-    }
-    
-    if(pagination){
-        const {limit, page} = pagination;
-        aggregateQuery.push({
-            $skip : page*limit
-        },{
-            $limit : limit
-        })
-    }
-
-    if(order_status){
-        aggregateQuery.push({
-            $match : {
-                order_status : order_status
-            }
-        })
-    }
-    
-    if(!aggregateQuery.length){
-        return await Transaction.find()
-    }
-        
-    let result = [];
-    
-    filter || pagination || order_status ? result = await Transaction.aggregate(aggregateQuery) : result = await Transaction.find().toArray();
-    return result;
-}
-
-const getOneTransactions = async(parent, {filter})=>{
-    if(!filter){
-        throw new Error('Nothing to find');
-    }else{
-        const {id} = filter;
-        let result = await Transaction.findOne({
-            _id : id
-        });
-        return result;
-    }
-}
-
-const deleteTransaction = async(parent, {input})=>{
-    if(!input){
-        throw new Error('Nothing to delete');
-    }else{
-        const {id} = input
-        let result = await Transaction.findByIdAndUpdate({
-            _id : id
-        },{
-            $set : {
-                status : 'deleted'
-            }
-        },{
-            new : true
-        });
-        return result;
-    }
-}
-
-const TransactionResolvers = {
-    Query : {
-        getAllTransactions,
-        getOneTransactions
-    },
-
-    Mutation : {
-        addCart,
-        deleteTransaction,
-        deleteMenu,
-        updateOrderStatus
-    },
-
-    Transactions : {
-        user_id : getUserLoader
-    },
-
-    Detail_Menu : {
-        recipe_id : getMenuLoader
-    }
+  }
+  return true;
 };
 
-module.exports = {TransactionResolvers}
+const validateStockIngredient = async menu => {
+  let recipeData = [];
+  let ingredientData = [];
+  let available = [];
+  for (const recipe of menu) {
+    recipeData = await Recipe.findOne({
+      _id: recipe.recipe_id
+    });
+    for (const ingredient of recipeData.ingredients) {
+      ingredientData = await Ingredient.findOne({
+        _id: ingredient.ingredient_id,
+        stock: {
+          $gte: 0
+        }
+      });
+      if (ingredientData.stock >= ingredient.stock_used * recipe.amount) {
+        available.push(true);
+      } else {
+        available.push(false);
+      }
+    }
+  }
+  const temp = available.includes(false);
+  if (temp === false) {
+    await reduceIngredientStock(menu);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const getUserLoader = async (parent, args, ctx) => {
+  if (parent.user_id) {
+    const result = await ctx.dataUserLoader.load(parent.user_id);
+    return result;
+  }
+};
+
+const getMenuLoader = async (parent, args, ctx) => {
+  if (parent.recipe_id) {
+    const result = await ctx.dataRecipeLoader.load(parent.recipe_id);
+    return result;
+  }
+};
+
+const getTotalPrice = async menu => {
+  let total = [];
+  let totalPrice = [];
+  for (recipe of menu) {
+    recipeData = await Recipe.findOne({
+      _id: recipe.recipe_id
+    });
+    total.push(recipe.amount * recipeData.price);
+    totalPrice = total.reduce((a, b) => a + b);
+  }
+  return totalPrice;
+};
+
+const updateMenu = async (id, menu) => {
+  let totalPrice = await getTotalPrice(menu);
+  const addMenu = await Transaction.findByIdAndUpdate(
+    {
+      _id: id
+    },
+    {
+      $push: {
+        menu: menu
+      }
+    },
+    {
+      new: true
+    }
+  );
+  if (addMenu) {
+    const updateTotal = await Transaction.findByIdAndUpdate(
+      {
+        _id: id
+      },
+      {
+        $inc: {
+          total: totalPrice
+        }
+      },
+      {
+        new: true
+      }
+    );
+    return updateTotal;
+  }
+};
+
+const createTransaction = async (id, menu) => {
+  let totalPrice = await getTotalPrice(menu);
+  if (id && menu) {
+    let data = new Transaction({
+      user_id: id,
+      menu: menu,
+      total: totalPrice,
+      order_status: "pending",
+      status: "active"
+    });
+    await data.save();
+    return data;
+  } else {
+    throw new Error("Cant create new transaction");
+  }
+};
+
+const addCart = async (parent, { input }, ctx) => {
+  const userId = ctx.user[0]._id;
+  const data = await Transaction.findOne({
+    user_id: mongoose.Types.ObjectId(userId),
+    order_status: "pending",
+    status: "active"
+  });
+  if (!input) {
+    throw new Error("No data to input");
+  } else {
+    const { menu } = input;
+    if (data == null) {
+      const create = await createTransaction(userId, menu);
+      return create;
+    } else {
+      const update = await updateMenu(data._id, menu);
+      return update;
+    }
+  }
+};
+
+const deleteMenu = async (parent, { input }, ctx) => {
+  if (!input) {
+    throw new Error("No menu to dalete");
+  } else {
+    const userId = ctx.user[0]._id;
+    const data = await Transaction.findOne({
+      user_id: mongoose.Types.ObjectId(userId),
+      order_status: "pending",
+      status: "active"
+    });
+    if (data) {
+      const { id } = input;
+      const deleteMenu = await Transaction.findByIdAndUpdate(
+        {
+          _id: data._id
+        },
+        {
+          $pull: {
+            menu: {
+              _id: mongoose.Types.ObjectId(id)
+            }
+          }
+        },
+        {
+          new: true
+        }
+      );
+      if (deleteMenu) {
+        let totalPrice = await getTotalPrice(deleteMenu.menu);
+        const updateTotal = await Transaction.findByIdAndUpdate(
+          {
+            _id: data._id
+          },
+          {
+            $set: {
+              total: totalPrice
+            }
+          },
+          {
+            new: true
+          }
+        );
+        return updateTotal;
+      }
+    }
+  }
+};
+
+const updateOrderStatus = async (parent, args, ctx) => {
+  const userId = ctx.user[0]._id;
+  const data = await Transaction.findOne({
+    user_id: mongoose.Types.ObjectId(userId),
+    order_status: "pending",
+    status: "active"
+  });
+
+  if (data != null) {
+    const validate = await validateStockIngredient(data.menu);
+    if (validate === true) {
+      const result = await Transaction.findByIdAndUpdate(
+        {
+          _id: data._id
+        },
+        {
+          $set: {
+            order_status: "success"
+          }
+        },
+        {
+          new: true
+        }
+      );
+      return result;
+    } else {
+      const result = await Transaction.findByIdAndUpdate(
+        {
+          _id: data._id
+        },
+        {
+          $set: {
+            order_status: "failed"
+          }
+        },
+        {
+          new: true
+        }
+      );
+      return result;
+    }
+  } else {
+    throw new Error("Cant update order status");
+  }
+};
+
+const getAllTransactions = async (
+  parent,
+  { filter, pagination, order_status },
+  ctx
+) => {
+  let aggregateQuery = [];
+  let matchQuerry = {
+    $and: [
+      {
+        status: "active"
+      }
+    ]
+  };
+
+  if (filter) {
+    if (filter.user_lname || filter.recipe_name) {
+      if (filter.user_lname) {
+        const search = new RegExp(filter.user_lname, "i");
+        aggregateQuery.push(
+          {
+            $lookup: {
+              from: "users",
+              localField: "user_id",
+              foreignField: "_id",
+              as: "user_detail"
+            }
+          },
+          {
+            $match: {
+              "user_detail.last_name": search
+            }
+          }
+        );
+      }
+
+      if (filter.recipe_name) {
+        const search = new RegExp(filter.recipe_name, "i");
+        aggregateQuery.push(
+          {
+            $lookup: {
+              from: "recipes",
+              localField: "menu.recipe_id",
+              foreignField: "_id",
+              as: "recipe_detail"
+            }
+          },
+          {
+            $match: {
+              "recipe_detail.recipe_name": search
+            }
+          }
+        );
+      }
+    }
+
+    if (filter.order_status) {
+      const search = new RegExp(filter.order_status, "i");
+      matchQuerry.$and.push({
+        order_status: search
+      });
+    }
+
+    if (filter.order_date) {
+      const search = new RegExp(filter.order_date, "i");
+      matchQuerry.$and.push({
+        order_date: search
+      });
+    }
+  }
+
+  if (matchQuerry.$and.length) {
+    aggregateQuery.push({
+      $match: matchQuerry
+    });
+  }
+
+  if (pagination) {
+    const { limit, page } = pagination;
+    aggregateQuery.push(
+      {
+        $skip: page * limit
+      },
+      {
+        $limit: limit
+      }
+    );
+  }
+
+  if (order_status) {
+    aggregateQuery.push({
+      $match: {
+        order_status: order_status
+      }
+    });
+  }
+
+  if (!aggregateQuery.length) {
+    return await Transaction.find();
+  }
+
+  let result = await Transaction.aggregate(aggregateQuery);
+  result = result.map(el => {
+    return {
+      ...el,
+      count: result.length
+    };
+  });
+  return result;
+};
+
+const getOneTransactions = async (parent, { filter }) => {
+  if (!filter) {
+    throw new Error("Nothing to find");
+  } else {
+    const { id } = filter;
+    let result = await Transaction.findOne({
+      _id: id
+    });
+    return result;
+  }
+};
+
+const deleteTransaction = async (parent, { input }) => {
+  if (!input) {
+    throw new Error("Nothing to delete");
+  } else {
+    const { id } = input;
+    let result = await Transaction.findByIdAndUpdate(
+      {
+        _id: id
+      },
+      {
+        $set: {
+          status: "deleted"
+        }
+      },
+      {
+        new: true
+      }
+    );
+    return result;
+  }
+};
+
+const TransactionResolvers = {
+  Query: {
+    getAllTransactions,
+    getOneTransactions
+  },
+
+  Mutation: {
+    addCart,
+    deleteTransaction,
+    deleteMenu,
+    updateOrderStatus
+  },
+
+  Transactions: {
+    user_id: getUserLoader
+  },
+
+  Detail_Menu: {
+    recipe_id: getMenuLoader
+  }
+};
+
+module.exports = { TransactionResolvers };
--- Transaction/transaction.typedefs.js
+++ Transaction/transaction.typedefs.js
@@ -1,81 +1,83 @@
-const {gql} = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const TransactionTypeDefs = gql`
-    # scalar Date
+  type Transactions {
+    _id: ID
+    user_id: Users
+    menu: [Detail_Menu]
+    total: Int
+    order_status: Order
+    order_date: String
+    status: Status
+    count: Int
+  }
 
-    type Transactions {
-        _id : ID,
-        user_id : Users,
-        menu : [Detail_Menu],
-        total : Int,
-        order_status : Order,
-        order_date : String,
-        status : Status
-    }
+  type Detail_Menu {
+    _id: ID
+    recipe_id: Recipes
+    amount: Int
+    note: String
+  }
 
-    type Detail_Menu {
-        _id : ID,
-        recipe_id : Recipes,
-        amount : Int,
-        note : String
-    }
+  enum Order {
+    success
+    failed
+    pending
+  }
 
-    enum Order {
-        success,
-        failed,
-        pending
-    }
+  enum Status {
+    active
+    deleted
+  }
 
-    enum Status {
-        active,
-        deleted
-    }
+  input DataDeleteTransaction {
+    id: ID
+  }
 
-    input DataDeleteTransaction {
-        id : ID
-    }
+  input DataInputTransaction {
+    menu: [Detail]
+  }
 
-    input DataInputTransaction {
-        menu : [Detail]
-    }
+  input Detail {
+    recipe_id: ID
+    amount: Int
+    note: String
+  }
 
-    input Detail {
-        recipe_id : ID,
-        amount : Int,
-        note : String
-    }
+  input DataFilterTransaction {
+    user_lname: String
+    recipe_name: String
+    order_status: Order
+    order_date: String
+  }
 
-    input DataFilterTransaction {
-        user_lname : String,
-        recipe_name : String,
-        order_status : Order,
-        order_date : String
-    }
+  input OneFilterTransaction {
+    id: ID
+  }
 
-    input OneFilterTransaction {
-        id : ID
-    }
+  input Paging {
+    page: Int
+    limit: Int
+  }
 
-    input Paging {
-        page : Int,
-        limit : Int
-    }
+  input DeleteMenu {
+    id: ID
+  }
 
-    input DeleteMenu {
-        id : ID
-    }
+  type Query {
+    getAllTransactions(
+      filter: DataFilterTransaction
+      pagination: Paging
+    ): [Transactions]
+    getOneTransactions(filter: OneFilterTransaction): Transactions
+  }
 
-    type Query {
-        getAllTransactions(filter : DataFilterTransaction, pagination : Paging, order_status : Order) : [Transactions],
-        getOneTransactions(filter : OneFilterTransaction) : Transactions
-    }
+  type Mutation {
+    addCart(input: DataInputTransaction): Transactions
+    deleteTransaction(input: DataDeleteTransaction): Transactions
+    deleteMenu(input: DeleteMenu): Transactions
+    updateOrderStatus: Transactions
+  }
+`;
 
-    type Mutation {
-        addCart(input : DataInputTransaction) : Transactions,
-        deleteTransaction(input : DataDeleteTransaction) : Transactions,
-        deleteMenu(input : DeleteMenu) : Transactions,
-        updateOrderStatus : Transactions
-    }
-`
-
-module.exports = {TransactionTypeDefs}
+module.exports = { TransactionTypeDefs };
