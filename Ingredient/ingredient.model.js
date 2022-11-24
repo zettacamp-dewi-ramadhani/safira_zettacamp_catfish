1a819dd49954604a5c71ea0024e42f16df15810a
zettacamp_dewi_ramadhani <safira.ramadhani@zettacamp.pro>
2022-11-24 13:09:38 +0700

    fix bug

--- Ingredient/ingredient.model.js
+++ Ingredient/ingredient.model.js
@@ -2,20 +2,20 @@ const {db, mongoose} = require('../Controller/database');
 const moment = require('moment')
 
 const ingredientSchema = new mongoose.Schema({
-    name : {
-        type: String,
-        unique : true
-    },
-    stock: {type: Number},
-    status : {
-        type: String,
-        enum : ['active', 'deleted'],
-        default : 'active'
-    },
-    created_at : {
-        type : Date,
-        default : Date.now()
-    }
+  name: {
+    type: String,
+    unique: true
+  },
+  stock: { type: Number },
+  status: {
+    type: String,
+    enum: ["active", "deleted"],
+    default: "active"
+  },
+  created_at: {
+    type: String,
+    default: Date.now()
+  }
 });
 
 const Ingredient = mongoose.model('ingredients', ingredientSchema);
--- Ingredient/ingredient.resolvers.js
+++ Ingredient/ingredient.resolvers.js
@@ -1,155 +1,169 @@
-const Ingredient = require('./ingredient.model');
-const Recipe = require('../Recipe/recipe.model')
+const Ingredient = require("./ingredient.model");
+const Recipe = require("../Recipe/recipe.model");
 
-const insertIngredient = async(parent, {input})=>{
-    if(!input){
-        throw new Error("Nothing to insert")
-    }else{
-        const {name, stock} = input;
-        let data = new Ingredient({
-            name : name,
-            stock : stock
-        });
-        await data.save();
-        return data;
-    }
-}
-
-const getAllIngredients = async(parent, {filter, paging})=>{
-    let aggregateQuery = [];
-    let matchQuerry = {
-        $and : [{
-            status: 'active'
-        }],
-    }
-    
-    let count = await Ingredient.count()
-    
-    if(filter){
-        if(filter.name){
-            const search = new RegExp(filter.name, 'i');
-            matchQuerry.$and.push({
-                name : search,
-            })
-        }
-    
-        if(filter.stock >=0){ 
-            if(filter.stock === 0) {
-                throw new Error ('Filter stock must greater then 0')
-            }else if (filter.stock === null){
-                throw new Error(`Filter stock can't null`)
-            }else{
-                matchQuerry.$and.push({
-                    stock : filter.stock,
-                })
-            }
-        }
-    }
-
-    if(matchQuerry.$and.length){
-        aggregateQuery.push({
-          $match: matchQuerry
-        })
-    }
-
-    if(paging){
-        const {limit, page} = paging;
-        aggregateQuery.push({
-            $sort : {
-                created_at : -1
-            }
-        },{
-            $skip : page*limit
-        },{
-            $limit : limit
-        })
-    }
-
-    if(!aggregateQuery.length){
-       let result = await Ingredient.find()
-        return result
-    }    
-    
-    let result = await Ingredient.aggregate(aggregateQuery)
-    result =  {
-        data: result,
-        TotalDocument : count,
-        countResult: result.length
-    }
-    return result
-}
-
-const getOneIngredient = async(parent,{filter})=>{
-    if(!filter){
-        throw new Error("Nothing to show");
-    }else{
-        const {id} = filter;
-        let result = await Ingredient.findOne({
-            _id : id,
-            status : 'active'
-        });
-        return result;
-    }
-}
-
-const updateIngredient = async(parent, {input})=>{
-    if(!input){
-        throw new Error("Nothing to update")
-    }else{
-        const {id, newStock} = input;
-        let data = await Ingredient.findByIdAndUpdate({
-            _id : id
-        },{
-            $set : {
-                stock : newStock
-            }
-        },{
-            new : true
-        });
-        return data;
-    }
-}
-
-const validateDelete = async(id)=>{
-    const result = await Recipe.find({
-        "ingredients.ingredient_id" : id
+const insertIngredient = async (parent, { input }) => {
+  if (!input) {
+    throw new Error("Nothing to insert");
+  } else {
+    const { name, stock } = input;
+    let data = new Ingredient({
+      name: name,
+      stock: stock
     });
-    return result
-}
+    await data.save();
+    return data;
+  }
+};
 
-const deleteIngredient = async(parent, {input},ctx)=>{
-    if(!input){
-        throw new Error('Input the data first')
-    }else{
-        const {id} = input;
-        const validate = await validateDelete(id);
-        if(validate == 0){
-            let result = await Ingredient.findByIdAndUpdate({
-                _id : id
-            },{
-                $set : {
-                    status : "deleted"
-                }
-            },{
-                new : true
-            })
-            return result
-        }else{
-            throw new Error('The Ingredients already use in recipes');
-        }
+const getAllIngredients = async (parent, { filter, paging }) => {
+  let aggregateQuery = [];
+  let matchQuerry = {
+    $and: [
+      {
+        status: "active"
+      }
+    ]
+  };
+
+  let count = await Ingredient.count();
+
+  if (filter) {
+    if (filter.name) {
+      const search = new RegExp(filter.name, "i");
+      matchQuerry.$and.push({
+        name: search
+      });
     }
-}
-const IngredientResolvers = {
-    Query : {
-        getAllIngredients,
-        getOneIngredient
+
+    if (filter.stock >= 0) {
+      if (filter.stock === 0) {
+        throw new Error("Filter stock must greater then 0");
+      } else if (filter.stock === null) {
+        throw new Error(`Filter stock can't null`);
+      } else {
+        matchQuerry.$and.push({
+          stock: filter.stock
+        });
+      }
+    }
+  }
+
+  if (matchQuerry.$and.length) {
+    aggregateQuery.push({
+      $match: matchQuerry
+    });
+  }
+
+  if (paging) {
+    const { limit, page } = paging;
+    aggregateQuery.push(
+      {
+          $skip: page * limit
     },
+    {
+        $limit: limit
+    },
+    {
+        $sort: {
+          created_at: -1
+        }
+      }
+    );
+  }
 
-    Mutation : {
-        insertIngredient,
-        updateIngredient,
-        deleteIngredient
+  if (!aggregateQuery.length) {
+    let result = await Ingredient.find();
+    return result;
+  }
+
+  let result = await Ingredient.aggregate(aggregateQuery);
+  result = {
+    data: result,
+    TotalDocument: count,
+    countResult: result.length
+  };
+  return result;
+};
+
+const getOneIngredient = async (parent, { filter }) => {
+  if (!filter) {
+    throw new Error("Nothing to show");
+  } else {
+    const { id } = filter;
+    let result = await Ingredient.findOne({
+      _id: id,
+      status: "active"
+    });
+    return result;
+  }
+};
+
+const updateIngredient = async (parent, { input }) => {
+  if (!input) {
+    throw new Error("Nothing to update");
+  } else {
+    const { id, newStock } = input;
+    let data = await Ingredient.findByIdAndUpdate(
+      {
+        _id: id
+      },
+      {
+        $set: {
+          stock: newStock
+        }
+      },
+      {
+        new: true
+      }
+    );
+    return data;
+  }
+};
+
+const validateDelete = async id => {
+  const result = await Recipe.find({
+    "ingredients.ingredient_id": id
+  });
+  return result;
+};
+
+const deleteIngredient = async (parent, { input }, ctx) => {
+  if (!input) {
+    throw new Error("Input the data first");
+  } else {
+    const { id } = input;
+    const validate = await validateDelete(id);
+    if (validate == 0) {
+      let result = await Ingredient.findByIdAndUpdate(
+        {
+          _id: id
+        },
+        {
+          $set: {
+            status: "deleted"
+          }
+        },
+        {
+          new: true
+        }
+      );
+      return result;
+    } else {
+      throw new Error("The Ingredients already use in recipes");
     }
-}
+  }
+};
+const IngredientResolvers = {
+  Query: {
+    getAllIngredients,
+    getOneIngredient
+  },
 
-module.exports =  {IngredientResolvers}
+  Mutation: {
+    insertIngredient,
+    updateIngredient,
+    deleteIngredient
+  }
+};
+
+module.exports = { IngredientResolvers };
