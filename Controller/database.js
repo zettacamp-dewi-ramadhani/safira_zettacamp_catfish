33e549d8c67fb55e5fd3829b6474b66c8b57afad
Dewi Safira Ramadhani <safira.ramadhani@zettacamp.pro>
2022-11-28 09:22:03 +0700

    fix bug

--- Controller/database.js
+++ Controller/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const db = mongoose
-  .connect("mongodb://localhost:27017/zettacamp_safira")
+  .connect("mongodb://localhost:27017/zettacamp")
   .then(() => console.log("Connected"))
   .catch(err => console.log(err));
 
