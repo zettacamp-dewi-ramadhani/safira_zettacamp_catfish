fde86f079cf883e390295fdedba8a3784e7ab992
zettacamp_dewi_ramadhani <safira.ramadhani@zettacamp.pro>
2022-11-30 10:59:48 +0700

    add return

--- User/user.resolvers.js
+++ User/user.resolvers.js
@@ -87,6 +87,8 @@ const login = async (parent, {input : {email, password}})=>{
                 user : {
                     _id : user._id,
                     email : user.email,
+                    first_name : user.first_name,
+                    last_name : user.last_name,
                     user_type : user.user_type
                 }
             }
