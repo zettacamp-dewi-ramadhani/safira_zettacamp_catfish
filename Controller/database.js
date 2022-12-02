bd094ec96aa28069921291aaa8d92bf0e15d49c9
zettacamp_dewi_ramadhani <safira.ramadhani@zettacamp.pro>
2022-12-02 11:07:14 +0700

    update connection

--- Controller/database.js
+++ Controller/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const db = mongoose
-  .connect("mongodb://localhost:27017/zettacamp_safira")
+  .connect("mongodb+srv://chocorange:miniproject@zettacamp.k2k8zav.mongodb.net/zettacamp?retryWrites=true&w=majority")
   .then(() => console.log("Connected"))
   .catch(err => console.log(err));
 
